ffe73d91051ce181fc6977eee61d4b6138d98ce9
Roninwing <tomasleoncano@gmail.com>
2025-11-14 10:16:56 +0100

    Mejora del texto del juego

--- Videojuego/src/videojuego/Main.java
+++ Videojuego/src/videojuego/Main.java
@@ -12,15 +12,22 @@ public class Main {
         
         
         System.out.println("Batalla en Gotham city");
-        System.out.println("En una noche de Gotham city... mientras caminas por la calle escuchas gruñidos y gritos");
-        System.out.println("de gente desesperada, vas a investigar por curiosidad, no es hasta que ahí te encuentras");
-        System.out.println("a Batman haciendo su trabajo interrogando lo a quien puedes asimilar que son gánsters");
-        System.out.println("de bajo nivel, no es hasta que Batman reconoce tu presencia y dice:\n" +
+        System.out.println("En una noche de Gotham city... mientras caminas por la calle escuchas gruñidos y gritos \n" +
+        "de gente desesperada, vas a investigar por curiosidad, no es hasta que ahí te encuentras \n" +
+        "a Batman haciendo su trabajo interrogando lo a quien puedes asimilar que son gánsters \n" +
+        "de bajo nivel, no es hasta que Batman reconoce tu presencia y dice: \n" +
         "\n" +
-        "B: ...Tu... has sido testigo de algo que no deberías de haber visto. Saben tu cara... Pero no tu nombre... ¿Quién eres?...");
+        "B: ...Tu... has sido testigo de algo que no deberías de haber visto. Saben tu cara... Pero no tu nombre... ¿Quién eres?..." );
         
-        System.out.println("introduce tu nombre ");
+        System.out.print("introduce tu nombre: ");
         nombreJugador = teclado.nextLine();
+
+        System.out.println("B: " +nombreJugador+ " Tu noche acaba de empeorar y espero \n" + 
+        "que te puedas manejar por las calles. Por que hoy me vas a acompañar... \n" +
+        "\n");
+
+        System.out.println("Es así como tu historia empieza para resolver el misterio que rodea esta noche en Gotham. \n" +
+        "\n");
         
         Jugador jugador1 = new Jugador(nombreJugador, 0, 0, 0);
         
